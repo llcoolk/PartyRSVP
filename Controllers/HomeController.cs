502b7b5923a5f3cfed1368f05eda365de7165713
Leen Kim <leenkim@alumni.usc.edu>
2019-07-09 14:58:46 -0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyRSVP.Models;
+
+namespace PartyRSVP.Controllers {
+    public class HomeController : Controller {
+        // public string Index () {
+        //     return "Hello World";
+        // }
+
+        public IActionResult Index () {
+            return View ();
+        }
+        public ViewResult Rsvp () {
+            return View ("Rsvp");
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm (GuestResponse guestResponse) {
+            // Todo: Store guest response, covered in later lessons
+            return View ("Thanks", guestResponse);
+        }
+
+        // public IActionResult Privacy () {
+        //     return View ();
+        // }
+
+        // [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error () {
+        //     return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // }
+    }
+}
